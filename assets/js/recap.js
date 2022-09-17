11cc50544a1ff06d786370018bc3eeae692d9d3e
Tusar <tusarahmed141@gmail.com>
2022-09-17 15:16:15 +0600

    exploring js object

--- assets/js/recap.js
+++ assets/js/recap.js
@@ -14,39 +14,55 @@ console.log(arrayNum1);
 
 // Destructuring
 const animal = {
-  name: 'dog',
-  color: 'brown',
-  age: 7
-}
+  name: "dog",
+  color: "brown",
+  age: 7,
+};
 
-const {name, color, age} = animal;
+const { name, color, age } = animal;
 
 console.log(name, color, age);
 
 // Rest parameter
 const car = {
-  carName: 'Fiat',
-  color: 'red',
-  price: '$20k'
-}
+  carName: "Fiat",
+  color: "red",
+  price: "$20k",
+};
 
-const {carName, ...rest} = car;
+const { carName, ...rest } = car;
 console.log(carName);
-
 
 // When object going to undefined
 const person = {
-  personName : 'Tusar',
-  eyeColor: 'Black',
-  age: 23
-}
+  personName: "Tusar",
+  eyeColor: "Black",
+  age: 23,
+};
 
-const {personName, ...personRest} = person || {};
+const { personName, ...personRest } = person || {};
 console.log(personName);
 
 // JavaScript Map
 const arrayNum3 = [1, 2, 3, 4, 5];
 console.log(...arrayNum3);
-arrayNum3.map(num => {
+arrayNum3.map((num) => {
   console.log(num * num);
-})
+});
+
+// Explore javascript filter method
+const products = [
+  { id: 1, name: "apple", price: 500, color: "golden" },
+  { id: 2, name: "xiaomi", price: 124, color: "black" },
+  { id: 3, name: "samsung", price: 200, color: "black" },
+  { id: 4, name: "samsung2", price: 200, color: "black" },
+  { id: 5, name: "lenovo", price: 300, color: "pink" },
+  { id: 6, name: "xiaomi", price: 100, color: "pink" },
+  { id: 7, name: "lenovo", price: 300, color: "pink" },
+];
+
+const needLenovo = products.filter(product => product.name == 'lenovo');
+console.log(needLenovo);
+
+const otherwisePink = products.filter(product => product.color != 'pink');
+console.log(otherwisePink);
