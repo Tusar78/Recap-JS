c8fa19bee2bf2246bdce297621d2d851cf2d79bf
Tusar <tusarahmed141@gmail.com>
2022-09-17 14:16:37 +0600

    Spread opreator is done

--- assets/js/recap.js
+++ assets/js/recap.js
@@ -1,5 +1,13 @@
 // Template String
-const str1 = 'Rakibul';
-const str2 = 'Islam';
+const str1 = "Rakibul";
+const str2 = "Islam";
 
 console.log(`Hello ${str1} ${str2}`);
+
+// Spread Operator
+const arrayNum1 = [1, 2, 3, 4, 5];
+const arrayNum2 = [6, 7, 8, 9];
+
+arrayNum1.push(...arrayNum2);
+
+console.log(arrayNum1);
