9d78825695ee5598d40e116911f6ed5af688e2a9
Tusar <tusarahmed141@gmail.com>
2022-09-17 14:25:26 +0600

    practice destructuring

--- assets/js/recap.js
+++ assets/js/recap.js
@@ -11,3 +11,14 @@ const arrayNum2 = [6, 7, 8, 9];
 arrayNum1.push(...arrayNum2);
 
 console.log(arrayNum1);
+
+// Destructuring
+const animal = {
+  name: 'dog',
+  color: 'brown',
+  age: 7
+}
+
+const {name, color, age} = animal;
+
+console.log(name, color, age);
